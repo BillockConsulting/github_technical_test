cbcb943c33d0d01c918093848011aee6b34a0041
Matt Billock <mattbillock@mattbillock.com>
2017-08-19 18:51:49 -0500

    Moved target repo into a class constant

--- lib/github_communicator.rb
+++ lib/github_communicator.rb
@@ -6,10 +6,11 @@ class GithubCommunicator
   GITHUB_ISSUE_ASSIGNMENT_TARGET = "evilstickman"
   GITHUB_REPO_NAME = ':owner/:repo'
   GITHUB_USER_ACCESS_TOKEN = ENV['github_access_token']
+  GITHUB_NOTIFICATION_REPO = 'notification_repo'
 
-  def create_issue_in_target_repo(organization, repo_deleted, repo_for_notification, message)
+  def create_issue_in_target_repo(organization, repo_deleted, message)
     octokit_client.user_agent = GITHUB_USER_AGENT
-    repo_path = GITHUB_REPO_NAME.gsub(':owner', organization).gsub(':repo',repo_for_notification)
+    repo_path = GITHUB_REPO_NAME.gsub(':owner', organization).gsub(':repo',GITHUB_NOTIFICATION_REPO)
     body_message = '@' + GITHUB_ISSUE_ASSIGNMENT_TARGET + " - " + message
     octokit_client.create_issue(repo_path, 
       GITHUB_ISSUE_TITLE_TEMPLATE.gsub(":repo", repo_deleted),
